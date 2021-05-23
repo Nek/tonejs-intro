739927d76bd83178c8be7f5dddec34189c89aa92
Nikita Dudnik <nikdudnik@gmail.com>
2021-05-23 19:32:35 +0200

    Volume & Panning

--- index.js
+++ index.js
@@ -7,13 +7,25 @@ document.getElementById("noise-maker").addEventListener("click", makeNoise);
 async function makeNoise() {
   //start up the audio machinery
   await Tone.start();
+
+  //create a panner devices. 1 is for rightmost position,
+  //-1 is for the leftmost one
+  const panner = new Tone.Panner(1).toDestination();
+
   //create an audio file player, load piano-loop.mp3 and make it looped
   const player = new Tone.Player({
     url: "piano-loop.mp3",
     loop: true,
-  }).toDestination();
+  });
+  //plug the player into the panner. Don't forget to remove toDestination from the player creation code
+  player.connect(panner);
+
   //wait for the sounds to load
   await Tone.loaded();
+  //fade it in during 10 seconds
+  player.fadeIn = 10;
   //play a sound
   player.start();
+  //use ramptTo to move the sound from right to left
+  panner.pan.rampTo(-1, 10);
 }
