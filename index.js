624b94db08b7457ac201b2ad255ac0d70f722f75
Nikita Dudnik <nikdudnik@gmail.com>
2021-05-23 19:17:42 +0200

    Make some noise

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hi!");
+import * as Tone from "tone";
+
+//this is needed due to the browser's security sandbox restrictions
+//sounds can start playing only after a user has started interacting with a page
+document.getElementById("noise-maker").addEventListener("click", makeNoise);
+
+async function makeNoise() {
+  //start up the audio machinery
+  await Tone.start();
+  //create a synth and connect it to the main output (your speakers)
+  const synth = new Tone.Synth().toDestination();
+  //play a middle 'C' for the duration of an 8th note
+  synth.triggerAttackRelease("C4", "8n");
+}
