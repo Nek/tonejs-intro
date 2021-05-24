eedfc9b87d4e2a1ed814fe104ad64257d9fd0300
Nikita Dudnik <nikdudnik@gmail.com>
2021-05-24 06:55:00 +0200

    Music & background noise

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import * as Tone from "tone";
+import { Tremolo } from "tone";
 
 //this is needed due to the browser's security sandbox restrictions
 //sounds can start playing only after a user has started interacting with a page
@@ -7,4 +8,22 @@ document.getElementById("noise-maker").addEventListener("click", makeNoise);
 async function makeNoise() {
   //start up the audio machinery
   await Tone.start();
+
+  const musicSpace = new Tone.Reverb(1).toDestination();
+
+  const music = new Tone.Player({
+    url: "piano-loop.mp3",
+    loop: true,
+  }).connect(musicSpace);
+  //this is in dB. it's a relative, not an absolute unit of loudness
+  //just experiment with the numbers
+  music.volume.value = -12;
+  const noise = new Tone.Player({
+    url: "chatter.mp3",
+    loop: true,
+  }).toDestination();
+
+  await Tone.loaded();
+  music.start();
+  noise.start();
 }
